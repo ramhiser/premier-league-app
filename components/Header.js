8c383de3a995373cadd4026f75f5bfe66e5d7be9
John Ramey <johnramey@gmail.com>
2021-08-16 22:54:49 -0500

    Premier League header

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="bg-premier-league-purple text-white font-sans p-5 pl-12 pt-4 shadow-lg">
+            <h1>Premier League</h1>
+        </header>
+    )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Used in Google's Premier League page
+        'premier-league-purple': '#3F1052',
+      }
+    }
   },
   variants: {
     extend: {},
