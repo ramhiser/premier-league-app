9c0d7360d0e06a7ca22aeefb46298732661bd56c
John Ramey <johnramey@gmail.com>
2021-08-21 14:34:50 -0500

    added team metadata and logos

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
       <Header />
       <Nav />
 
+      {/* 1:33 */}
       <ActivePage />
 
       {/* Results */}
---
+++ utils/teams.js
@@ -0,0 +1,104 @@
+export default {
+    "arsenal": {
+        name: "Arsenal",
+        long_name: "Arsenal F.C.",
+        logo: "arsenal.png" 
+    },
+    "aston-villa": {
+        name: "Aston Villa",
+        long_name: "Aston Villa F.C.",
+        logo: "aston-villa.png"
+    },
+    "brentford": {
+        name: "Brentford",
+        long_name: "Brentford F.C.",
+        logo: "brentford.png"
+    },
+    "brighton": {
+        name: "Brighton",
+        long_name: "Brighton & Hove Albion F.C.",
+        logo: "https://ssl.gstatic.com/onebox/media/sports/logos/EKIe0e-ZIphOcfQAwsuEEQ_48x48.png"
+    },
+    "burnley": {
+        name: "Burnley",
+        long_name: "Burnley F.C.",
+        logo: "burnley.png"
+    },
+    "chelsea": {
+        name: "Chelsea",
+        long_name: "Chelsea F.C.",
+        logo: "chelsea.png"
+    },
+    "crystal-palace": {
+        name: "Crystal Palace",
+        long_name: "Crystal Palace F.C.",
+        logo: "crystal-palace.png"
+    },
+    "everton": {
+        name: "Everton",
+        long_name: "Everton F.C.",
+        logo: "everton.png"
+    },
+    "leeds": {
+        name: "Leeds United",
+        long_name: "Leeds United F.C.",
+        logo: "leeds-united.png"
+    },
+    "leicester-city": {
+        name: "Leicester City",
+        long_name: "Leicester City F.C.",
+        logo: "leicester-city.png"
+    },
+    "liverpool": {
+        name: "Liverpool",
+        long_name: "Liverpool F.C.",
+        logo: "liverpool.png"
+    },
+    "manchester-city": {
+        name: "Manchester City",
+        long_name: "Manchester City F.C.",
+        logo: "manchester-city.png"
+    },
+    "manchester-united": {
+        name: "Manchester United",
+        long_name: "Manchester United F.C.",
+        logo: "manchester-united.png"
+    },
+    "newcastle": {
+        name: "Newcastle",
+        long_name: "Newcastle F.C.",
+        logo: "newcastle.png"
+    },
+    "norwich-city": {
+        name: "norwich-city",
+        long_name: "norwich-city F.C.",
+        logo: "norwich-city.png"
+    },
+    "southampton": {
+        name: "Southampton",
+        long_name: "Southampton F.C.",
+        logo: "southampton.png"
+    },
+    "tottenham": {
+        name: "Tottenham",
+        long_name: "Tottenham Hotspur F.C.",
+        logo: "tottenham.png"
+    },
+    "watford": {
+        name: "Watford",
+        long_name: "Watford F.C.",
+        logo: "watford.png"
+    },
+    "west-ham": {
+        name: "West Ham",
+        long_name: "West Ham United F.C.",
+        logo: "west-ham.png"
+    },
+    "wolves": {
+        name: "Wolves",
+        long_name: "Wolverhampton Wanderers F.C.",
+        logo: "wolves.png"
+    }    
+}
+
+_
