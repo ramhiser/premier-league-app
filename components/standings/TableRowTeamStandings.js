0cc98534774900ec55a705400d1a35d2633d0261
John Ramey <johnramey@gmail.com>
2021-08-22 02:32:50 -0500

    fixed some of standings margin issues

--- components/standings/TableRowTeamStandings.js
+++ components/standings/TableRowTeamStandings.js
@@ -6,24 +6,26 @@ function TableRowTeamStandings({standings, team_rank}) {
     console.log(standings);
     const team_data = teams[standings.team];
     return (
-        <div className="grid grid-cols-11 bg-white h-10 border-t border-b border-standings-border-gray">
-            <div>{team_rank + 1}</div>
-            <div className="min-w-321">
+        <div className="grid grid-cols-11 text-sm bg-white hover:bg-gray-50 h-10 border-t border-b border-standings-border-gray">
+            <div className="pl-2 m-auto text-right">{team_rank + 1}</div>
+            <div className="m-auto w-max min-w-321">
                 <Image
                     src={`/logos/${team_data.logo}`}
                     height={24}
                     width={24}
                 />
-                {team_data.name}
+                <div className="whitespace-nowrap">
+                    {team_data.name}
+                </div>
             </div>
-            <div>{standings.matches_played}</div>
-            <div>{standings.wins}</div>
-            <div>{standings.draws}</div>
-            <div>{standings.losses}</div>
-            <div>{standings.goals_for}</div>
-            <div>{standings.goals_against}</div>
-            <div>{standings.goal_difference}</div>
-            <div>{standings.points}</div>
+            <div className="m-auto">{standings.matches_played}</div>
+            <div className="m-auto text-left">{standings.wins}</div>
+            <div className="m-auto">{standings.draws}</div>
+            <div className="m-auto">{standings.losses}</div>
+            <div className="m-auto">{standings.goals_for}</div>
+            <div className="m-auto">{standings.goals_against}</div>
+            <div className="m-auto">{standings.goal_difference}</div>
+            <div className="m-auto">{standings.points}</div>
             {/* TODO: Last 5 matches */}
             <div></div>
         </div>
--- components/standings/TableSeasonStandings.js
+++ components/standings/TableSeasonStandings.js
@@ -2,20 +2,20 @@ import TableRowTeamStandings from './TableRowTeamStandings'
 
 function TableSeasonStandings({standings}) {
     return (
-        <div className="flex flex-col font-sans bg-white items-center justify-center align-text-middle">
-            <div className="grid grid-cols-11 text-sm h-10 text-standings-header-gray border-t-2 border-standings-border-gray">
-                <div className="min-w-321 col-span-2 text-left pl-4">Club</div>
-                <div className="min-w-32">MP</div>
-                <div className="min-w-32">W</div>
-                <div className="min-w-32">D</div>
-                <div className="min-w-32">L</div>
-                <div className="min-w-32">GF</div>
-                <div className="min-w-32">GA</div>
-                <div className="min-w-32">GD</div>
-                <div className="min-w-32">Pts</div>
-                <div className="min-w-32">Last 5</div>
+        <div className="font-sans bg-white items-center justify-center align-text-middle">
+            <div className="grid grid-cols-11 align-middle text-sm h-10 text-standings-header-gray border-t-2 border-l-2 border-r-2 border-standings-border-gray">
+                <div className="col-span-2  m-auto min-w-321 text-left pl-4">Club</div>
+                <div className="m-auto">MP</div>
+                <div className="m-auto">W</div>
+                <div className="m-auto">D</div>
+                <div className="m-auto">L</div>
+                <div className="m-auto">GF</div>
+                <div className="m-auto">GA</div>
+                <div className="m-auto">GD</div>
+                <div className="m-auto">Pts</div>
+                <div className="m-auto">Last 5</div>
             </div>
-            <div className="bg-white h-10 border-t border-b border-standings-border-gray">
+            <div className="bg-white h-10 border-t border-b border-l-2 border-r-2 border-standings-border-gray">
                 {standings.map((team_standings, index) => (
                     <TableRowTeamStandings key={team_standings.team} standings={team_standings} team_rank={index} />
                 ))}
